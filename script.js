a9c4c9fb5e239626dc02138560c9071d5645f6e7
Wesley Monteiro de Araujo <wesleymonteirodearaujo@gmail.com>
2023-04-03 17:30:38 -0300

    att

---
+++ script.js
@@ -0,0 +1,37 @@
+var userPadrao = 'admin'
+var senhaPadrao = 'admin'
+var userCad = localStorage.getItem('userCad');
+var passCad = localStorage.getItem('passCad');
+var passConf
+
+/* alert(`usuario e senha padrão (${userPadrao}, ${senhaPadrao})`) */
+
+function logar() {
+    var user = document.getElementById('login').value
+    var pass = document.getElementById('senha').value
+    if (user == userPadrao && pass == senhaPadrao) {
+        alert(`Bem vindo, ${user}`)
+    } else if (user == userCad && pass == passCad) {
+        alert(`Bem vindo, ${user}`)
+    } else {
+        alert('Usuario e senha Não encontrados')
+    }
+}
+
+function cadastrar() {
+    userCad = document.getElementById('loginCad').value
+    passCad = document.getElementById('senha1').value
+    passConf = document.getElementById('senha2').value
+    if (passCad == passConf) {
+        alert(`Usuario e Senha Cadastrados ${userCad} ${passCad}`)
+        localStorage.setItem('userCad', userCad);
+        localStorage.setItem('passCad', passCad);
+        const url = "index.html";
+        window.location.href = url;
+    } else {
+        alert('Senha não confere...')
+        document.getElementById('senha1').value = "";
+        document.getElementById('senha2').value = "";
+    }
+}
+
